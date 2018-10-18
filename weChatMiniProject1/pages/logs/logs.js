7d6646423d69a38513790f92979cc89cd19a375a
TencentIT <1969368719@qq.com>
2018-10-18 17:27:52 +0800

    10.18 微信小程序demo1

---
+++ weChatMiniProject1/pages/logs/logs.js
@@ -0,0 +1,13 @@
+//logs.js
+const util = require('../../utils/util.js')
+
+Page({
+  data: {
+    logs: []
+  },
+  onLoad: function () {
+    console.log(new Date())
+    console.log(wx)
+
+  }
+})
